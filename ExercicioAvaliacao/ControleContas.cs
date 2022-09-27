616b0289190abeef2a3488090bb66c7f79f8a0f1
MARCELA GAMA QUINTANILHA <marcela.gquintanilha@senacsp.edu.br>
2022-09-27 07:31:16 -0300

    socorro

--- ExercicioAvaliacao/ControleContas.cs
+++ ExercicioAvaliacao/ControleContas.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -30,5 +31,56 @@ namespace ExercicioAvaliacao
             //contasreceber.MdiParent = this;
             contasreceber.Show();   
         }
+
+        void MostrarPagar()
+        {
+            try
+            {
+                using (MySqlConnection cnx = new MySqlConnection())
+                {
+                    cnx.ConnectionString = "server = localhost; database = controle; uid = root; pwd =; port = 3306;Convert Zero DateTime = true";
+                    cnx.Open();
+                    string sql = "select * from contas where tipo=1 && pago_recebido=0";     // vai mostrar na dataGridView as contas que NÃO foram pagas
+                    DataTable table = new DataTable();
+                    MySqlDataAdapter adapter = new MySqlDataAdapter(sql, cnx);
+                    adapter.Fill(table);
+                    dgwContasPagar.DataSource = table;
+                    dgwContasPagar.AutoGenerateColumns = false;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+
+
+
+
+
+        void MostrarReceber()
+        {
+            try
+            {
+                using (MySqlConnection cnx = new MySqlConnection())
+                {
+                    cnx.ConnectionString = "server = localhost; database = controle; uid = root; pwd =; port = 3306;Convert Zero DateTime = true";
+                    cnx.Open();
+                    string sql = "select * from contas where tipo=0 && pago_recebido=0";     // vai mostrar na dataGridView as contas que NÃO foram pagas
+                    DataTable table = new DataTable();
+                    MySqlDataAdapter adapter = new MySqlDataAdapter(sql, cnx);
+                    adapter.Fill(table);
+                    dgwContasPagar.DataSource = table;
+                    dgwContasPagar.AutoGenerateColumns = false;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+
     }
 }
